a051fc28544b23e47bdce2949b20a9b0522f3ab6
Ian Schmitz <ianschmitz@gmail.com>
2023-11-21 21:16:40 -0800

    Improve keybinds

---
+++ modules/home-manager/dotfiles/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+
+      -- Override the default settings to prevent it from selecting the first item by default,
+      -- and the enter key from automatically selecting an item if not selected.
+      -- This makes using enter to create newline in insert mode more intuitive.
+      opts.completion = {
+        completeopt = "menu,menuone,noinsert,noselect",
+      }
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<S-CR>"] = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = false,
+        }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      })
+    end,
+  },
+}
