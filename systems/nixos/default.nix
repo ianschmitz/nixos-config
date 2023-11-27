d7390604fe9e0c49a3c4e9af8766913dab512731
Ian Schmitz <ianschmitz@gmail.com>
2023-11-27 13:41:17 -0800

    nixos: start ssh agent

--- systems/nixos/default.nix
+++ systems/nixos/default.nix
@@ -38,6 +38,8 @@
     xclip
   ];
 
+  programs.ssh.startAgent = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
