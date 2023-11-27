e0946c39b2841d4c7b74308cd28b17f235ccfb5c
Ian Schmitz <ianschmitz@gmail.com>
2023-11-27 13:32:58 -0800

    nixos: add xclip
    Adds clipboard support needed by neovim

--- systems/nixos/default.nix
+++ systems/nixos/default.nix
@@ -35,8 +35,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     git
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    xclip
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
